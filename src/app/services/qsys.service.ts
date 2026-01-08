667cc4ab6d195ca6af8a0d69e160c96287397fff
rod-driscoll <rod@theavitgroup.com.au>
2026-01-08 17:14:50 +1100

    Fix: set qrwc ws options to default to secure

--- src/app/services/qsys.service.ts
+++ src/app/services/qsys.service.ts
@@ -49,7 +49,7 @@ export class QSysService {
     if (typeof optionsOrIp === 'string') {
       this.options = {
         coreIp: optionsOrIp,
-        secure: false,
+        secure: true,
         pollInterval: 35,
       };
     } else {
